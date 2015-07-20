0d9868c473551551b9f80a9f88783c6e4c672f81
Louis Popi <theh2o64@gmail.com>
2015-07-20 17:02:11 +0200

    falcon : Update proprietary files BUILD REQUEST INFO: SW Version: peregrine_retbr-user 5.1 LPB23.13-35 35 release-keysM8926_309101.02.03.17R MBM Version: 41.1A Modem Version: M8926_309101.02.03.17R FSG Version: FSG-DVXLTE-02.02.12 Build Fingerprint: motorola/peregrine_retbr/peregrine:5.1/LPB23.13-35/35:user/release-keys
    VERSION INFO FOUND UNDER 'ABOUT PHONE' SCREEN:
    System Version: 23.21.35.peregrine_retbr.retbr.en.BR
    Model number: Moto G
    Android Version: 5.1
    Baseband Version: M8926_309101.02.03.17R
    Build Number: LPB23.13-35
    Build Date: Mon Jun  8 09:23:25 CDT 2015
    
    OTHER MISC VERSION INFO:
    Subsidy Lock Config: None
    Blur Version: Blur_Version.23.21.35.peregrine_retbr.retbr.en.BR
    Version when read from CPV: peregrine_retbr-user 5.1 LPB23.13-35 35 release-keys

--- falcon-vendor-blobs.mk
+++ falcon-vendor-blobs.mk
@@ -202,6 +202,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/falcon/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libril-qc-qmi-1.so:system/vendor/lib/libril-qc-qmi-1.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
+    vendor/motorola/msm8226-common/proprietary/vendor/lib/libsystem_health_mon.so:system/vendor/lib/libsystem_health_mon.so \
     vendor/motorola/falcon/proprietary/vendor/lib/libxml.so:system/vendor/lib/libxml.so \
     vendor/motorola/falcon/proprietary/bin/thermal-engine:system/bin/thermal-engine \
     vendor/motorola/falcon/proprietary/vendor/lib/libthermalclient.so:system/vendor/lib/libthermalclient.so \
