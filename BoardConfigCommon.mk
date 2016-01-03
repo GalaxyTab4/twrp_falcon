6e74bbe1351dcc823b7735c6f053aae22a8eb965
sub77 <sub77@ymail.com>
2016-01-03 18:36:24 +0100

    twrp: update kernel cmd line

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,7 @@ TARGET_NO_BOOTLOADER := true
 RECOVERY_VERSION := 20160103
 
 TARGET_PREBUILT_KERNEL := device/motorola/msm8226-common/kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.bootdevice=msm_sdcc.1 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
